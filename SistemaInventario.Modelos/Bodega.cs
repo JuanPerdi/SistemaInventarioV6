ba249a575171bc6fd69cfe125dc4f6e2c7036c16
JuanPerdi <juanperdigueros11@gmail.com>
2023-11-04 12:12:01 +0100

    Creación modelo Bodega

---
+++ SistemaInventario.Modelos/Bodega.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInventario.Modelos
+{
+    public class Bodega
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Nombre es requerido")]
+        [MaxLength(60,ErrorMessage ="Nombre con 60 caracteres máximo")] 
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "Descripción es requerido")]
+        [MaxLength(100, ErrorMessage = "Descripción con 60 caracteres máximo")]
+        public string Descripcion { get; set; }
+        public bool Estado { get; set;}
+    }
+}

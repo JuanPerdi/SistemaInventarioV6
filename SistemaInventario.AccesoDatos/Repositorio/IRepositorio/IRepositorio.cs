3a14188f47751682d335a56295bb2485ab5a3687
JuanPerdi <juanperdigueros11@gmail.com>
2023-11-04 20:50:31 +0100

    Implementada primera parte de Repositorio.cs

---
+++ SistemaInventario.AccesoDatos/Repositorio/IRepositorio/IRepositorio.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInventario.AccesoDatos.Repositorio.IRepositorio
+{
+    public interface IRepositorio<T> where T : class
+    {
+        //Task<T> lo utilizamos para que sean métodos asíncronos
+        Task<T> Obtener(int id);
+        Task<IEnumerable<T>> ObtenerTodos(
+            Expression<Func<T,bool>> filtro=null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy=null,
+            string incluirPropiedades=null,
+            bool isTracking=true
+            );
+
+        Task<T> ObtenerPrimero(
+            Expression<Func<T, bool>> filtro = null,
+            string incluirPropiedades = null,
+            bool isTracking = true
+            );
+
+        Task Agregar(T entidad);
+        void Remove(T entidad);
+        void RemoverRango(IEnumerable<T> entidad);
+    }
+}
---
+++ SistemaInventario.AccesoDatos/Repositorio/Repositorio.cs
@@ -0,0 +1,43 @@
+﻿using SistemaInventario.AccesoDatos.Repositorio.IRepositorio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInventario.AccesoDatos.Repositorio
+{
+    public class Repositorio<T> : IRepositorio<T> where T : class
+    {
+        public void Agregar(T entidad)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> Obtener(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> ObtenerPrimero(Expression<Func<T, bool>> filtro = null, string incluirPropiedades = null, bool isTracking = true)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> ObtenerTodos(Expression<Func<T, bool>> filtro = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, string incluirPropiedades = null, bool isTracking = true)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(T entidad)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoverRango(IEnumerable<T> entidad)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

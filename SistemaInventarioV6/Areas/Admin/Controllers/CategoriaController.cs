bb2b90292b86235280fc066c1a1ed1fe06884832
JuanPerdi <juanperdigueros11@gmail.com>
2023-11-09 12:07:07 +0100

    CRUD de Marca

---
+++ SistemaInventarioV6/Areas/Admin/Controllers/CategoriaController.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SistemaInventario.AccesoDatos.Repositorio.IRepositorio;
+using SistemaInventario.Modelos;
+using SistemaInventario.Utilidades;
+
+namespace SistemaInventarioV6.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoriaController : Controller
+    {
+        private readonly IUnidadTrabajo _unidadTrabajo;
+        public CategoriaController(IUnidadTrabajo unidadTrabajo)
+        {
+                _unidadTrabajo=unidadTrabajo;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public async Task<IActionResult> Upsert(int? id)
+        {
+            //? significa que pueude ser nulo
+            Categoria categoria = new Categoria();
+            if (id == null)
+            {
+                //nueva bodega
+                categoria.Estado = true;
+                return View(categoria);
+            }
+            //Actualizar Bodega
+            categoria = await _unidadTrabajo.Categoria.Obtener(id.GetValueOrDefault());//si el id es nulo que no de error
+            if (categoria == null)
+            {
+                return NotFound();
+            }
+            return View(categoria);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]//evitar falsifiación de solicitudes
+        public async Task<IActionResult> Upsert (Categoria categoria)
+        {
+            if(ModelState.IsValid) //comrpueba si el modelo es válido en todas sus propiedades
+            {
+                if (categoria.Id==0) //Nuevo registro
+                {
+                    await _unidadTrabajo.Categoria.Agregar(categoria);
+                    TempData[DS.Exitosa] = "Categoria creada exitósamente";
+                }
+                else //Actualizar registro
+                {
+                    _unidadTrabajo.Categoria.Actualizar(categoria);
+                    TempData[DS.Exitosa] = "Categoria actualizada exitósamente";
+
+                }
+                await _unidadTrabajo.Guardar();
+                return RedirectToAction(nameof(Index));
+            }
+            TempData[DS.Error] = "Error al grabar Categoria";
+            return View(categoria);
+        }
+
+        #region API
+        //esto es porque se van a llamar mediante JS
+        [HttpGet]
+        public async Task<IActionResult> ObtenerTodos()
+        {
+            var todos = await _unidadTrabajo.Categoria.ObtenerTodos();
+            return Json(new {data=todos});
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var categoriaDB = await _unidadTrabajo.Categoria.Obtener(id);
+            if(categoriaDB == null)
+            {
+                return Json(new { succes = false, message = "Error al borrar la Categoria" });
+            }
+            _unidadTrabajo.Categoria.Remover(categoriaDB);//no ponemos await porque no es un método asíncrono
+            await _unidadTrabajo.Guardar();
+            return Json(new { success = true, message = "Categoria borrada exitósamente" });
+        }
+
+        [ActionName("ValidarNombre")]
+        public async Task<IActionResult> ValidarNombre(string nombre,int id = 0)
+        {
+            bool valor = false;
+            var lista = await _unidadTrabajo.Categoria.ObtenerTodos();
+            if (id == 0)
+            {
+                valor=lista.Any(b=>b.Nombre.ToLower().Trim()==nombre.ToLower().Trim());
+            }
+            else
+            {
+                valor = lista.Any(b => b.Nombre.ToLower().Trim() == nombre.ToLower().Trim() && b.Id != id);
+            }
+            if (valor)
+            {
+                return Json(new { data = true });
+            }
+            return Json(new {data=false});
+        }
+
+        #endregion
+    }
+}
---
+++ SistemaInventarioV6/Areas/Admin/Controllers/MarcaController.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SistemaInventario.AccesoDatos.Repositorio.IRepositorio;
+using SistemaInventario.Modelos;
+using SistemaInventario.Utilidades;
+
+namespace SistemaInventarioV6.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class MarcaController : Controller
+    {
+        private readonly IUnidadTrabajo _unidadTrabajo;
+        public MarcaController(IUnidadTrabajo unidadTrabajo)
+        {
+                _unidadTrabajo=unidadTrabajo;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public async Task<IActionResult> Upsert(int? id)
+        {
+            //? significa que pueude ser nulo
+            Marca marca = new Marca();
+            if (id == null)
+            {
+                //nueva bodega
+                marca.Estado = true;
+                return View(marca);
+            }
+            //Actualizar Bodega
+            marca = await _unidadTrabajo.Marca.Obtener(id.GetValueOrDefault());//si el id es nulo que no de error
+            if (marca == null)
+            {
+                return NotFound();
+            }
+            return View(marca);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]//evitar falsifiación de solicitudes
+        public async Task<IActionResult> Upsert (Marca marca)
+        {
+            if(ModelState.IsValid) //comrpueba si el modelo es válido en todas sus propiedades
+            {
+                if (marca.Id==0) //Nuevo registro
+                {
+                    await _unidadTrabajo.Marca.Agregar(marca);
+                    TempData[DS.Exitosa] = "Marca creada exitósamente";
+                }
+                else //Actualizar registro
+                {
+                    _unidadTrabajo.Marca.Actualizar(marca);
+                    TempData[DS.Exitosa] = "Marca actualizada exitósamente";
+
+                }
+                await _unidadTrabajo.Guardar();
+                return RedirectToAction(nameof(Index));
+            }
+            TempData[DS.Error] = "Error al grabar Marca";
+            return View(marca);
+        }
+
+        #region API
+        //esto es porque se van a llamar mediante JS
+        [HttpGet]
+        public async Task<IActionResult> ObtenerTodos()
+        {
+            var todos = await _unidadTrabajo.Marca.ObtenerTodos();
+            return Json(new {data=todos});
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var marcaDB = await _unidadTrabajo.Marca.Obtener(id);
+            if(marcaDB == null)
+            {
+                return Json(new { succes = false, message = "Error al borrar la Marca" });
+            }
+            _unidadTrabajo.Marca.Remover(marcaDB);//no ponemos await porque no es un método asíncrono
+            await _unidadTrabajo.Guardar();
+            return Json(new { success = true, message = "Marca borrada exitósamente" });
+        }
+
+        [ActionName("ValidarNombre")]
+        public async Task<IActionResult> ValidarNombre(string nombre,int id = 0)
+        {
+            bool valor = false;
+            var lista = await _unidadTrabajo.Marca.ObtenerTodos();
+            if (id == 0)
+            {
+                valor=lista.Any(b=>b.Nombre.ToLower().Trim()==nombre.ToLower().Trim());
+            }
+            else
+            {
+                valor = lista.Any(b => b.Nombre.ToLower().Trim() == nombre.ToLower().Trim() && b.Id != id);
+            }
+            if (valor)
+            {
+                return Json(new { data = true });
+            }
+            return Json(new {data=false});
+        }
+
+        #endregion
+    }
+}

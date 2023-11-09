6e051bf462d6f2aed35f79bff7eddc97c187df48
JuanPerdi <juanperdigueros11@gmail.com>
2023-11-09 18:32:47 +0100

    Listado de Productos

--- SistemaInventario.AccesoDatos/Data/ApplicationDbContext.cs
+++ SistemaInventario.AccesoDatos/Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using SistemaInventario.Modelos;
+using SistemaInventario.Modelos.ViewModels;
 using System.Reflection;
 
 namespace SistemaInventario.AccesoDatos.Data
@@ -15,6 +16,7 @@ namespace SistemaInventario.AccesoDatos.Data
         public DbSet<Bodega> Bodegas { get; set; }
         public DbSet<Categoria> Categorias { get; set; }
         public DbSet<Marca> Marcas { get; set; }
+        public DbSet<Producto> Productos { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
---
+++ SistemaInventarioV6/Areas/Admin/Controllers/ProductoController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SistemaInventario.AccesoDatos.Repositorio.IRepositorio;
+using SistemaInventario.Modelos;
+using SistemaInventario.Utilidades;
+
+namespace SistemaInventarioV6.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductoController : Controller
+    {
+        private readonly IUnidadTrabajo _unidadTrabajo;
+        public ProductoController(IUnidadTrabajo unidadTrabajo)
+        {
+                _unidadTrabajo=unidadTrabajo;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public async Task<IActionResult> Upsert(int? id)
+        {
+            return View();
+        }
+
+       
+
+        #region API
+        //esto es porque se van a llamar mediante JS
+        [HttpGet]
+        public async Task<IActionResult> ObtenerTodos()
+        {
+            var todos = await _unidadTrabajo.Producto.ObtenerTodos(incluirPropiedades:"Categoria,Marca");
+            return Json(new {data=todos});
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var poductoDB = await _unidadTrabajo.Producto.Obtener(id);
+            if(poductoDB == null)
+            {
+                return Json(new { succes = false, message = "Error al borrar el Producto" });
+            }
+            _unidadTrabajo.Producto.Remover(poductoDB);//no ponemos await porque no es un método asíncrono
+            await _unidadTrabajo.Guardar();
+            return Json(new { success = true, message = "Producto borrado exitósamente" });
+        }
+
+        [ActionName("ValidarSerie")]
+        public async Task<IActionResult> ValidarSerie(string serie,int id = 0)
+        {
+            bool valor = false;
+            var lista = await _unidadTrabajo.Producto.ObtenerTodos();
+            if (id == 0)
+            {
+                valor=lista.Any(b=>b.NumeroSerie.ToLower().Trim()==serie.ToLower().Trim());
+            }
+            else
+            {
+                valor = lista.Any(b => b.NumeroSerie.ToLower().Trim() == serie.ToLower().Trim() && b.Id != id);
+            }
+            if (valor)
+            {
+                return Json(new { data = true });
+            }
+            return Json(new {data=false});
+        }
+
+        #endregion
+    }
+}

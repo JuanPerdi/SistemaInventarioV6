829347233d17d10ef7d21760271d1a29001a4519
JuanPerdi <juanperdigueros11@gmail.com>
2023-11-27 12:56:10 +0100

    Clase 101

--- SistemaInventario.Modelos/ViewModels/InventarioVM.cs
+++ SistemaInventario.Modelos/ViewModels/InventarioVM.cs
@@ -12,7 +12,7 @@ namespace SistemaInventario.Modelos.ViewModels
     {
         public Inventario Inventario { get; set; }
         public InventarioDetalle InventarioDetalle { get; set; }
-        public List<InventarioDetalle> InventarioDetalles { get; set; }
+        public IEnumerable<InventarioDetalle> InventarioDetalles { get; set; }
         public IEnumerable<SelectListItem> BodegaLista { get; set; }
     }
 }
--- SistemaInventarioV6/Areas/Inventario/Controllers/InventarioController.cs
+++ SistemaInventarioV6/Areas/Inventario/Controllers/InventarioController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using SistemaInventario.AccesoDatos.Repositorio.IRepositorio;
+using SistemaInventario.Modelos.ViewModels;
 using SistemaInventario.Utilidades;
+using System.Security.Claims;
 
 namespace SistemaInventarioV6.Areas.Inventario.Controllers
 {
@@ -11,9 +13,59 @@ namespace SistemaInventarioV6.Areas.Inventario.Controllers
     {
         private readonly IUnidadTrabajo _unidadTrabajo;
 
+        [BindProperty]//se utiliza para enlazar automáticamente los datos de la interfaz
+                      //de usuario (por ejemplo, formularios web) a propiedades específicas en el modelo de vista.
+        public InventarioVM inventarioVM { get; set; }
+
         public InventarioController(IUnidadTrabajo unidadTrabajo)
         {
             _unidadTrabajo = unidadTrabajo;
+        }
+        public IActionResult NuevoInventario()
+        {
+            inventarioVM = new InventarioVM()
+            {
+                Inventario = new SistemaInventario.Modelos.Inventario(),
+                BodegaLista = _unidadTrabajo.Inventario.ObtenerTodosDropDownLista("Bodega")
+
+            };
+            inventarioVM.Inventario.Estado = false;
+            //Obtener el ID del Usuario desde la sesión
+            var claimIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimIdentity.FindFirst(ClaimTypes.NameIdentifier);
+            inventarioVM.Inventario.UsuarioAplicacionId = claim.Value;
+            inventarioVM.Inventario.FechaInicial = DateTime.Now;
+            inventarioVM.Inventario.FechaFinal=DateTime.Now;
+
+            return View(inventarioVM);
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> NuevoInventario(InventarioVM inventarioVM)
+        {
+            if(ModelState.IsValid)
+            {
+                inventarioVM.Inventario.FechaInicial= DateTime.Now;
+                inventarioVM.Inventario.FechaFinal= DateTime.Now;
+                await _unidadTrabajo.Inventario.Agregar(inventarioVM.Inventario);
+                await _unidadTrabajo.Guardar();
+                return RedirectToAction("DetalleInventario", new { id = inventarioVM.Inventario.Id });
+
+            }
+            inventarioVM.BodegaLista = _unidadTrabajo.Inventario.ObtenerTodosDropDownLista("Bodega");
+            return View(inventarioVM);
+        }
+
+        public async Task<IActionResult> DetalleInventario(int id)
+        {
+            inventarioVM = new InventarioVM();
+            inventarioVM.Inventario = await _unidadTrabajo.Inventario.ObtenerPrimero(i => i.Id == id, incluirPropiedades: "Bodega");
+            inventarioVM.InventarioDetalles = await _unidadTrabajo.InventarioDetalle.ObtenerTodos(d => d.InventarioId == id,
+                incluirPropiedades: "Producto,Producto.Marca");
+
+            return View(inventarioVM);
         }
         public IActionResult Index()
         {
